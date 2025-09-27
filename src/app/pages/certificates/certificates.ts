f583cd9d08ba60da88431e399abc0d373e40c756
cmarinho-dev <cmarinho.dev@gmail.com>
2025-09-27 10:37:48 -0300

    first commit

---
+++ src/app/pages/certificates/certificates.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SecondaryButton } from '../../_components/secondary-button/secondary-button';
+import { CertificateItem } from '../../_components/certificate-item/certificate-item';
+
+@Component({
+  selector: 'app-certificates',
+  imports: [SecondaryButton, CertificateItem],
+  templateUrl: './certificates.html',
+  styleUrl: './certificates.css'
+})
+export class Certificates {
+
+}
